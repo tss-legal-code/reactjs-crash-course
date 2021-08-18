c6e3e046c255643edd4643d68bf65ac9a6ecc103
tss-legal-code <tss.legal.code@gmail.com>
2021-08-18 19:02:22 +0300

    task deletion works

--- src/App.js
+++ src/App.js
@@ -3,10 +3,53 @@ import Tasks from "./components/Tasks";
 import { useState } from 'react' // initailly - for Tasks
 
 function App() {
+    // context API, Redux///
+    const [tasks, setTasks] = useState(
+        [
+            {
+                id: 1,
+                text: "doctor's appointment",
+                day: "17.08.2021",
+                reminder: true,
+            },
+            {
+                id: 2,
+                text: "beer festival",
+                day: "21.08.2021",
+                reminder: true,
+            },
+            {
+                id: 3,
+                text: "buy car",
+                day: "23.08.2021",
+                reminder: false,
+            },
+            {
+                id: 4,
+                text: "swimmingpool",
+                day: "29.08.2021",
+                reminder: true,
+            },
+
+        ]
+
+    );
+
+    // delete task
+
+    const deleteTask = (id) => {
+        setTasks(
+            tasks.filter(
+                element => element.id !== id
+            )
+        )
+
+    }
+
     return (
         <div className="container">
-            <Header  />
-            <Tasks /> 
+            <Header />
+            <Tasks tasks={tasks} onDelete={deleteTask} />
         </div>
     );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import {FaTimes} from "react-icons/fa"
 
-const Task = () => {
+const Task = ({task, onDelete}) => {
+    // console.log(onDelete);
     return (
-        <div>
-            
+        <div className="task">
+            <h3>{task.text} <FaTimes  style={{color: "red", cursor: "pointer"}}  onClick={() => onDelete(task.id)} /></h3>
+            <p>{task.day}</p>
         </div>
     )
 }
 
-export default Task;
+export default Task
