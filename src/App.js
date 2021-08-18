071ddddd135431969007bd45c782680a5a50db07
tss-legal-code <tss.legal.code@gmail.com>
2021-08-18 15:18:01 +0300

    just before moving tasksList from TasksComponent into AppComponent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import { useState } from 'react' // initailly - for Tasks
 
 function App() {
     return (
         <div className="container">
             <Header  />
-            <Tasks />
+            <Tasks /> 
         </div>
     );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,42 +1,46 @@
 import React from 'react'
-import {useState} from 'react'
-
-
-const tasks = [
-    {
-        id: 1,
-        text: "doctor's appointment",
-        day: "17.08.2021",
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: "beer festival",
-        day: "21.08.2021",
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: "buy car",
-        day: "23.08.2021",
-        reminder: false,
-    },
-    {
-        id: 4,
-        text: "swimmingpool",
-        day: "29.08.2021",
-        reminder: true,
-    },
-
-]
-
+import { useState } from 'react'
 
 const Tasks = () => {
+
+    // context API, Redux///
+    const [tasks, setTasks] = useState(
+        [
+            {
+                id: 1,
+                text: "doctor's appointment",
+                day: "17.08.2021",
+                reminder: true,
+            },
+            {
+                id: 2,
+                text: "beer festival",
+                day: "21.08.2021",
+                reminder: true,
+            },
+            {
+                id: 3,
+                text: "buy car",
+                day: "23.08.2021",
+                reminder: false,
+            },
+            {
+                id: 4,
+                text: "swimmingpool",
+                day: "29.08.2021",
+                reminder: true,
+            },
+
+        ]
+
+    );
+
+
     return (
         <>
             {tasks.map(
-                (task) => 
-                (<h1 key={task.id}>{task.id} - {task.text}</h1>)
+                (task) =>
+                    (<h1 key={task.id}>{task.id} - {task.text}</h1>)
             )}
         </>
     )
